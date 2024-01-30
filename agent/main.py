22c9e64d30643be9c562c95923f588af964316f8
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-01-30 20:53:16 -0300

    pass current SLY_EXTRA_CERTS on restart if it is not changed

--- agent/main.py
+++ agent/main.py
@@ -159,13 +159,18 @@ def _start_net_client(docker_api=None):
             )
         else:
             try:
-                net_client_networks_dict = net_container.attrs.get("NetworkSettings").get("Networks")
+                net_client_networks_dict = net_container.attrs.get("NetworkSettings").get(
+                    "Networks"
+                )
                 net_client_network_name = list(net_client_networks_dict.keys())[0]
 
                 if net_client_network_name != constants.NET_CLIENT_NETWORK():
                     os.environ[constants._NET_CLIENT_NETWORK] = net_client_network_name
             except Exception as e:
-                sly.logger.fatal("Couldn't fetch network name from the net-client to reuse the same network", exc_info=True)
+                sly.logger.fatal(
+                    "Couldn't fetch network name from the net-client to reuse the same network",
+                    exc_info=True,
+                )
                 raise e
 
 
@@ -233,6 +238,8 @@ def init_envs():
         container_info = get_container_info()
         if new_ca_cert_path and constants.SLY_EXTRA_CA_CERTS() != new_ca_cert_path:
             new_envs[constants._SLY_EXTRA_CA_CERTS] = new_ca_cert_path
+        else:
+            new_envs[constants._SLY_EXTRA_CA_CERTS] = constants.SLY_EXTRA_CA_CERTS()
         runtime = (
             "nvidia" if restart_with_nvidia_runtime else container_info["HostConfig"]["Runtime"]
         )
@@ -265,7 +272,9 @@ def init_envs():
                     for k, v in envs_changes.items()
                 },
                 "volumes_changes": volumes_changes,
-                "runtime_changes": {container_info["HostConfig"]["Runtime"]: runtime} if restart_with_nvidia_runtime else {},
+                "runtime_changes": {container_info["HostConfig"]["Runtime"]: runtime}
+                if restart_with_nvidia_runtime
+                else {},
                 "ca_cert_changed": bool(new_ca_cert_path),
             },
         )
