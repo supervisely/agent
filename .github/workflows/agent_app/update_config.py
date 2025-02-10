3112637cf4f46144af98c042fbf5a1d00544a544
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-02-10 15:13:18 -0300

    workflow fix

--- .github/workflows/agent_app/update_config.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-
-this_dir = os.path.dirname(os.path.abspath(__file__))
-config = json.load(open(f"{this_dir}/config.json", "r"))
-config["docker_image"] = os.getenv("DOCKER_IMAGE")
-json.dump(config, open(f"{this_dir}/config.json", "w"))
