94bc1055e8a136ff49b9b102cbe4644637afabc5
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-08-11 13:49:29 -0300

    Simplify container removal logic in stop_task method by removing None check

--- agent/worker/agent.py
+++ agent/worker/agent.py
@@ -318,13 +318,12 @@ class Agent:
                 task.join(timeout=20)
                 task.terminate()
                 if isinstance(task, TaskDockerized):
-                    if task._container is not None:
-                        try:
-                            task._container.remove(force=True)
-                        except docker.errors.NotFound:
-                            pass
-                        except docker.errors.APIError as e:
-                            self.logger.error("Unable to stop the container", exc_info=True)
+                    try:
+                        task._container.remove(force=True)
+                    except docker.errors.NotFound:
+                        pass
+                    except:
+                        self.logger.error("Unable to stop the container", exc_info=True)
 
                 task_extra = {
                     "task_id": task_id,
